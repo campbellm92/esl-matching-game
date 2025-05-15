b0112ae26f7527490b73cd4ec0c71203b0669f30
Matthew Campbell <m.campbell92@gmail.com>
2025-05-15 17:21:33 +0200

    import hook, map cards

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,25 +1,17 @@
+import { useWordCards } from "../hooks/useWordCards";
 import Card from "./Card";
 
 export default function Game() {
+  const wordsPath = "../../data/a1-words.json";
+  const { cards, loading } = useWordCards(wordsPath);
+
   return (
     <div className="flex justify-center items-center min-h-screen">
       <div className="grid grid-cols-3 grid-rows-3 md:grid-cols-4 md:grid-rows-4 gap-2">
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
+        {loading && <div>Loading...</div>}
+        {cards.map((card) => (
+          <Card key={card.id} content={card.content} type={card.type} />
+        ))}
       </div>
     </div>
   );
