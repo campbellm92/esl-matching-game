3a946e02cc4d012ecf909d774234c94ef875a375
Matthew Campbell <m.campbell92@gmail.com>
2025-05-15 17:21:16 +0200

    add logic to style based on type of card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,9 +1,17 @@
-export default function Card() {
+import { cleanDefinition } from "../utils/cleanDefinition";
+
+export default function Card({ content, type }) {
+  let styles =
+    "flex justify-center items-center text-center text-slate-100 bg-slate-800 size-52 m-2 px-2 rounded-md cursor-pointer";
+  if (type === "word") {
+    styles += " text-3xl";
+  } else if (type === "definition") {
+    styles += " text-md";
+  }
+
   return (
     <>
-      <div className="flex justify-center items-center  text-3xl text-slate-100 bg-slate-800 size-52 m-2 rounded-md cursor-pointer">
-        Hello
-      </div>
+      <div className={styles}>{cleanDefinition(content)}</div>
     </>
   );
 }
