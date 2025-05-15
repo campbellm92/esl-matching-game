df27c533cee46c57b16c987a0fca98f9efe416ec
Matthew Campbell <m.campbell92@gmail.com>
2025-05-15 15:39:58 +0200

    initial component config

---
+++ src/components/Card.jsx
@@ -0,0 +1,9 @@
+export default function Card() {
+  return (
+    <>
+      <div className="flex justify-center items-center  text-3xl text-slate-100 bg-slate-800 size-52 m-2 rounded-md cursor-pointer">
+        Hello
+      </div>
+    </>
+  );
+}
